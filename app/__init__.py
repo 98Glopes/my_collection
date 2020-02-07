8fe9ca5eb429deb7a60fb463e779889fa1144e16
98Glopes <gabriel.lopes@outlook.com>
2020-02-06 21:46:51 -0300

    add postgres suport

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask (__name__)
 
 
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'sqlite.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kcvktcfoyswdpi:75362912a7624ad5fc303836733ffb867cf1e4c1fa0a8baa05cfa8df283545e5@ec2-3-213-192-58.compute-1.amazonaws.com:5432/dcnf0af9aa6l8c'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 cors = CORS(app)
@@ -22,5 +22,3 @@ db = SQLAlchemy(app)
 from app.catalog.views import catalog
 app.register_blueprint(catalog)
 
-if not os.path.exists('sqlite:////' + os.path.join(basedir, 'sqlite.db')):
-    db.create_all()
